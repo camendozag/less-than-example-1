32e96e14832e644276c4df1880889886e4790465
camendozag <112561581+camendozag@users.noreply.github.com>
2022-10-18 08:43:59 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,53 @@
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    game.over(false)
+})
+sprites.onDestroyed(SpriteKind.Projectile, function (sprite) {
+    info.changeScoreBy(1)
+})
+let projectile: Sprite = null
+let IPlay = sprites.create(img`
+    . . 7 7 . . . . . . . . . . . . 
+    . . 7 7 7 7 7 . . . . . . . . . 
+    . . . 7 7 7 7 7 . . . . . . . . 
+    . . . 7 7 7 7 7 7 . . . . . . . 
+    . . . 7 7 7 7 7 7 7 7 . . . . . 
+    . . . 7 7 7 7 7 7 7 7 . . . . . 
+    . . . 7 7 7 7 7 7 7 7 . . . . . 
+    . . . 7 7 7 7 7 7 . . . . . . . 
+    . . . 7 7 7 7 7 . . . . . . . . 
+    . . 7 7 7 7 7 . . . . . . . . . 
+    . . 7 7 7 7 . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(IPlay, 0, 100)
+IPlay.x = 8
+IPlay.setStayInScreen(true)
+info.setScore(0)
+game.onUpdateInterval(1000, function () {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . c c . . . . . . . 
+        . . . . c a a a a . . . . . . . 
+        . . . . a a f f b a . . . . . . 
+        . . . c a b f f c b . . . . . . 
+        . . . c b b b a f c b . . . . . 
+        . . . c b a c a b b b . . . . . 
+        . . . . b b f f a a c . . . . . 
+        . . . . . a a b b c . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, -150, 0)
+    projectile.setPosition(scene.screenWidth(), randint(0, scene.screenHeight()))
+    if (info.score() < 10) {
+        projectile.vx += 100
+    }
+})
